acbe96da9c1da5e7bb04b5079d96cba0122a4322
Bernavschi Natalia <natb@list.ru>
2024-02-06 10:20:10 +0200

    Add pagination to TestItemColumn List

--- app/Http/Controllers/TestItemColumnController.php
+++ app/Http/Controllers/TestItemColumnController.php
@@ -4,15 +4,160 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\TestItemColumn;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class TestItemColumnController extends Controller
 {
-    public static function index() {
-        $testItemColumns =  TestItemColumn::all();
+    // public static function index() {
+    //     $testItemColumns =  TestItemColumn::all();
+    //     return response()->json([
+    //         'status' => 200,
+    //         'testItemColumns' => $testItemColumns,
+    //     ]);
+    // }
+
+    public static function index(Request $request) {
+
+        $search = $request->query('search');
+        $sortColumn = $request->query('sortColumn');
+        $sortOrder = $request->query('sortOrder');
+        $page = $request->query('page', 1);
+        $perPage = $request->query('perPage', 10);
+        $filterTopic = $request->query('filterTopic');
+        $filterTheme = $request->query('filterTheme');
+        $filterProgram = $request->query('filterProgram');
+        $filterChapter = $request->query('filterChapter');
+        $filterTeacher = $request->query('filterTeacher');
+        $filterTestItem = $request->query('filterTestItem');
+        
+    
+        $allowedColumns = ['id', 'order_number', 'task', 'title', 'type', 'topic_name','status'];
+    
+        if (!in_array($sortColumn, $allowedColumns)) {
+            $sortColumn = 'id';
+        }
+    
+        $columnTableMapping = [
+            'id' => 'TTC',
+            'order_number' => 'TTC',  
+            'title' => 'TTC',          
+            'task' => 'TI',            
+            'type' => 'TI',
+            'topic_name' => 'VTT',
+            'status' => 'TTC',
+        ];
+    
+        $sqlTemplate = "
+        SELECT
+            TTC.id,
+            TTC.order_number,
+            TTC.title,
+            TTC.test_item_id,
+            TI.task,
+            TI.type,
+            VTT.topic_name,
+            VTT.teacher_id,
+            VTT.teacher_topic_id,
+            TH.chapter_id, 
+            LP.id AS program_id,
+            TTC.status
+        FROM 
+        	test_item_columns TTC
+            INNER JOIN test_items TI ON TTC.test_item_id = TI.id
+            INNER JOIN (
+                SELECT 
+                    TT.id,
+                    TT.id AS teacher_topic_id, 
+                    TT.topic_id,             
+                    TT.name AS topic_name,
+                	TT.teacher_id
+                FROM teacher_topics TT
+            ) AS VTT ON VTT.id = TI.teacher_topic_id
+            INNER JOIN topics ON VTT.topic_id = topics.id    
+            INNER JOIN theme_learning_programs TLP ON TLP.id = topics.theme_learning_program_id
+            INNER JOIN themes TH ON TLP.theme_id = TH.id
+            INNER JOIN learning_programs LP ON TLP.learning_program_id = LP.id
+        WHERE true
+        ";
+    
+    
+        $searchConditions = '';
+        if ($search) {
+            $searchLower = strtolower($search);
+    
+            $hiddenVariants = ['i','d','e','n','hi', 'hid', 'id', 'idd', 'dd','dde', 'hidd', 'hidde', 'de', 'den', 'en'];
+            $shownVariants = ['s','o','w','sh','ho','sho', 'show', 'wn', 'ow', 'own'];
+    
+            if ($searchLower === 'hidden' || in_array($searchLower, $hiddenVariants)) {
+                foreach ($allowedColumns as $column) {
+                    $table = $columnTableMapping[$column];
+                    $searchConditions .= ($column === 'status') ? "$table.$column = 1 OR " : "LOWER($table.$column) LIKE '%$searchLower%' OR ";
+                }
+            } elseif ($searchLower === 'shown' || in_array($searchLower, $shownVariants)) {
+                foreach ($allowedColumns as $column) {
+                    $table = $columnTableMapping[$column];
+                    $searchConditions .= ($column === 'status') ? "$table.$column = 0 OR " : "LOWER($table.$column) LIKE '%$searchLower%' OR ";
+                }
+            } else {
+                foreach ($allowedColumns as $column) {
+                    $table = $columnTableMapping[$column];
+                    $searchConditions .= "LOWER($table.$column) LIKE '%$searchLower%' OR ";
+                }
+            }
+            $searchConditions = rtrim($searchConditions, ' OR ');
+        }
+    
+        $sqlWithSortingAndSearch = $sqlTemplate;
+
+        if ($filterTeacher) {
+            $sqlWithSortingAndSearch .= " AND VTT.teacher_id = $filterTeacher";
+        }
+
+        if ($filterChapter) {
+            $sqlWithSortingAndSearch .= " AND TH.chapter_id = $filterChapter";
+        }
+    
+        if ($searchConditions) {
+            $sqlWithSortingAndSearch .= " AND $searchConditions";
+        }
+
+        if ($filterTopic) {
+            $sqlWithSortingAndSearch .= " AND VTT.teacher_topic_id = $filterTopic";
+        }
+
+        if ($filterTestItem) {
+            $sqlWithSortingAndSearch .= " AND TTC.test_item_id = $filterTestItem";
+        }
+
+        if ($filterTheme) {
+            $sqlWithSortingAndSearch .= " AND TLP.theme_id = $filterTheme";
+        }
+    
+        if ($filterProgram) {
+            $sqlWithSortingAndSearch .= " AND LP.id = $filterProgram";
+        }
+
+        $sqlWithSortingAndSearch .= " ORDER BY $sortColumn $sortOrder";
+
+        $totalResults = DB::select("SELECT COUNT(*) as total FROM ($sqlWithSortingAndSearch) as countTable")[0]->total;
+    
+        $lastPage = ceil($totalResults / $perPage);
+    
+        $offset = ($page - 1) * $perPage;
+    
+        $rawResults = DB::select("$sqlWithSortingAndSearch LIMIT $perPage OFFSET $offset");
+    
         return response()->json([
             'status' => 200,
-            'testItemColumns' => $testItemColumns,
+            'testItemColumns' => $rawResults,
+            'pagination' => [
+                'last_page' => $lastPage,
+                'current_page' => $page,
+                'from' => $offset + 1,
+                'to' => min($offset + $perPage, $totalResults),
+                'total' => $totalResults,
+            ],
         ]);
     }
 
